a3dceaa0c217f651b7a229df724193231321df3c
cakir-enes <caki-enes@outlook.com>
2020-03-24 01:36:12 +0300

    Initial Commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by Enes Cakir on 24.03.2020.
+//  Copyright © 2020 lymitshn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
