d27f152b7421015d280035f1296efdef6859981b
cakir-enes <cakir-enes@outlook.com>
2020-03-26 12:20:25 +0300

    shiny

--- raytracer/hittable_list.hpp
+++ raytracer/hittable_list.hpp
@@ -9,25 +9,29 @@
 #ifndef hittable_list_hpp
 #define hittable_list_hpp
 
-#include "hittable.h"
 #include <memory>
 #include <vector>
 
-using std::shared_ptr;
+#include "hittable.h"
+
 using std::make_shared;
+using std::shared_ptr;
 
 class hittable_list : public hittable {
-public:
-    hittable_list() {}
-    hittable_list(shared_ptr<hittable> object) { add(object); }
-    
-    void clear() { objects.clear(); }
-    void add(shared_ptr<hittable> object) { objects.push_back(object); }
-    
-    virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const;
-    
-public:
-    std::vector<shared_ptr<hittable>> objects;
+ public:
+  hittable_list() {}
+  hittable_list(shared_ptr<hittable> object) { add(object); }
+
+  void clear() { objects.clear(); }
+  void add(shared_ptr<hittable> object) { objects.push_back(object); }
+
+  virtual bool hit(const ray& r,
+                   double t_min,
+                   double t_max,
+                   hit_record& rec) const;
+
+ public:
+  std::vector<shared_ptr<hittable>> objects;
 };
 
 #endif /* hittable_list_hpp */
